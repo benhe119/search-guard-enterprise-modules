0b98528a7a5974fcfd0765d9145d24b90a6bc534
floragunn <dev@search-guard.com>
2018-06-07 13:20:14 +0200

    fix custom_attr_maxval_len name

--- src/main/java/com/floragunn/dlic/auth/ldap/util/ConfigConstants.java
+++ src/main/java/com/floragunn/dlic/auth/ldap/util/ConfigConstants.java
@@ -63,7 +63,7 @@ public final class ConfigConstants {
     public static final String LDAPS_ENABLED_SSL_PROTOCOLS = "enabled_ssl_protocols";
     
     //custom attributes
-    public static final String LDAP_CUSTOM_ATTR_MAXVAL_LEN = "custom_attrmaxval_len";
+    public static final String LDAP_CUSTOM_ATTR_MAXVAL_LEN = "custom_attr_maxval_len";
     public static final String LDAP_CUSTOM_ATTR_WHITELIST = "custom_attr_whitelist";
 
     private ConfigConstants() {
