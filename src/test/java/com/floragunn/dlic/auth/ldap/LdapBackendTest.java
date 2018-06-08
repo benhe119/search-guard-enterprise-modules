0601f1d2fbc8993431b7e3a41de5581deacbfefe
floragunn <dev@search-guard.com>
2018-06-08 10:02:48 +0200

    Delete test suite and move nondn role test. Otherwise it needs an additional start of the ldap server which slows down the tests (again).

--- src/test/java/com/floragunn/dlic/auth/ldap/LdapBackendTest.java
+++ src/test/java/com/floragunn/dlic/auth/ldap/LdapBackendTest.java
@@ -780,6 +780,35 @@ public class LdapBackendTest {
     
     }
     
+    @Test
+    public void testLdapAuthorizationNonDNRoles() throws Exception {
+
+        final Settings settings = Settings.builder()
+                .putList(ConfigConstants.LDAP_HOSTS, "localhost:" + EmbeddedLDAPServer.ldapPort)
+                .put(ConfigConstants.LDAP_AUTHC_USERSEARCH, "(uid={0})")
+                .put(ConfigConstants.LDAP_AUTHC_USERBASE, "ou=people,o=TEST")
+                .put(ConfigConstants.LDAP_AUTHZ_ROLEBASE, "ou=groups,o=TEST")
+                .put(ConfigConstants.LDAP_AUTHZ_ROLENAME, "cn")
+                .put(ConfigConstants.LDAP_AUTHZ_RESOLVE_NESTED_ROLES, true)
+                .put(ConfigConstants.LDAP_AUTHZ_ROLESEARCH, "(uniqueMember={0})")
+                .put(ConfigConstants.LDAP_AUTHZ_USERROLENAME, "description, ou") // no memberOf OID
+                .put(ConfigConstants.LDAP_AUTHZ_ROLESEARCH_ENABLED, true)
+                .build();
+
+        final User user = new User("nondnroles");
+
+        new LDAPAuthorizationBackend(settings, null).fillRoles(user, null);
+
+        Assert.assertNotNull(user);
+        Assert.assertEquals("nondnroles", user.getName());
+        Assert.assertEquals(5, user.getRoles().size());
+        Assert.assertTrue("Roles do not contain non-LDAP role 'kibanauser'", user.getRoles().contains("kibanauser"));
+        Assert.assertTrue("Roles do not contain non-LDAP role 'humanresources'", user.getRoles().contains("humanresources"));
+        Assert.assertTrue("Roles do not contain LDAP role 'dummyempty'", user.getRoles().contains("dummyempty"));
+        Assert.assertTrue("Roles do not contain non-LDAP role 'role2'", user.getRoles().contains("role2"));
+        Assert.assertTrue("Roles do not contain non-LDAP role 'anotherrole' from second role name", user.getRoles().contains("anotherrole"));
+    }
+    
     @AfterClass
     public static void tearDown() throws Exception {
 
