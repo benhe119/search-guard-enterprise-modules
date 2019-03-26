e1ab7c62cf8ee527d438d7a904f52b67c1765d15
Nils Bandener <git@bndnr.de>
2019-03-26 08:11:50 +0100

    Reformatted code

--- src/main/java/com/floragunn/searchguard/dlic/rest/api/ActionGroupsApiAction.java
+++ src/main/java/com/floragunn/searchguard/dlic/rest/api/ActionGroupsApiAction.java
@@ -48,165 +48,155 @@ import com.google.common.base.Strings;
 
 public class ActionGroupsApiAction extends PatchableResourceApiAction {
 
-	protected final Logger log = LogManager.getLogger(this.getClass());
+    protected final Logger log = LogManager.getLogger(this.getClass());
 
-	@Inject
-	public ActionGroupsApiAction(final Settings settings, final Path configPath, final RestController controller,
-			final Client client, final AdminDNs adminDNs, final IndexBaseConfigurationRepository cl,
-			final ClusterService cs, final PrincipalExtractor principalExtractor, final PrivilegesEvaluator evaluator,
-			ThreadPool threadPool, AuditLog auditLog) {
-		super(settings, configPath, controller, client, adminDNs, cl, cs, principalExtractor, evaluator, threadPool,
-				auditLog);
+    @Inject
+    public ActionGroupsApiAction(final Settings settings, final Path configPath, final RestController controller, final Client client,
+            final AdminDNs adminDNs, final IndexBaseConfigurationRepository cl, final ClusterService cs, final PrincipalExtractor principalExtractor,
+            final PrivilegesEvaluator evaluator, ThreadPool threadPool, AuditLog auditLog) {
+        super(settings, configPath, controller, client, adminDNs, cl, cs, principalExtractor, evaluator, threadPool, auditLog);
 
-		// legacy mapping for backwards compatibility
-		// TODO: remove in SG7
-		controller.registerHandler(Method.GET, "/_searchguard/api/actiongroup/{name}", this);
-		controller.registerHandler(Method.GET, "/_searchguard/api/actiongroup/", this);
-		controller.registerHandler(Method.DELETE, "/_searchguard/api/actiongroup/{name}", this);
-		controller.registerHandler(Method.PUT, "/_searchguard/api/actiongroup/{name}", this);
+        // legacy mapping for backwards compatibility
+        // TODO: remove in SG7
+        controller.registerHandler(Method.GET, "/_searchguard/api/actiongroup/{name}", this);
+        controller.registerHandler(Method.GET, "/_searchguard/api/actiongroup/", this);
+        controller.registerHandler(Method.DELETE, "/_searchguard/api/actiongroup/{name}", this);
+        controller.registerHandler(Method.PUT, "/_searchguard/api/actiongroup/{name}", this);
 
-		// corrected mapping, introduced in SG6
-		controller.registerHandler(Method.GET, "/_searchguard/api/actiongroups/{name}", this);
-		controller.registerHandler(Method.GET, "/_searchguard/api/actiongroups/", this);
-		controller.registerHandler(Method.DELETE, "/_searchguard/api/actiongroups/{name}", this);
-		controller.registerHandler(Method.PUT, "/_searchguard/api/actiongroups/{name}", this);
-		controller.registerHandler(Method.PATCH, "/_searchguard/api/actiongroups/", this);
-		controller.registerHandler(Method.PATCH, "/_searchguard/api/actiongroups/{name}", this);
+        // corrected mapping, introduced in SG6
+        controller.registerHandler(Method.GET, "/_searchguard/api/actiongroups/{name}", this);
+        controller.registerHandler(Method.GET, "/_searchguard/api/actiongroups/", this);
+        controller.registerHandler(Method.DELETE, "/_searchguard/api/actiongroups/{name}", this);
+        controller.registerHandler(Method.PUT, "/_searchguard/api/actiongroups/{name}", this);
+        controller.registerHandler(Method.PATCH, "/_searchguard/api/actiongroups/", this);
+        controller.registerHandler(Method.PATCH, "/_searchguard/api/actiongroups/{name}", this);
 
-	}
+    }
 
-	@Override
-	protected AbstractConfigurationValidator getValidator(final RestRequest request, BytesReference ref,
-			Object... param) {
-		return new ActionGroupValidator(request, ref, this.settings, param);
-	}
+    @Override
+    protected AbstractConfigurationValidator getValidator(final RestRequest request, BytesReference ref, Object... param) {
+        return new ActionGroupValidator(request, ref, this.settings, param);
+    }
 
-	@Override
-	protected Endpoint getEndpoint() {
-		return Endpoint.ACTIONGROUPS;
-	}
+    @Override
+    protected Endpoint getEndpoint() {
+        return Endpoint.ACTIONGROUPS;
+    }
 
-	@Override
-	protected String getResourceName() {
-		return "actiongroup";
-	}
+    @Override
+    protected String getResourceName() {
+        return "actiongroup";
+    }
 
-	@Override
-	protected String getConfigName() {
-		return ConfigConstants.CONFIGNAME_ACTION_GROUPS;
-	}
+    @Override
+    protected String getConfigName() {
+        return ConfigConstants.CONFIGNAME_ACTION_GROUPS;
+    }
 
-	@Override
-	protected void consumeParameters(final RestRequest request) {
-		request.param("application");
-		request.param("name");
-	}
+    @Override
+    protected void consumeParameters(final RestRequest request) {
+        request.param("application");
+        request.param("name");
+    }
 
-	@Override
-	protected void handleGet(final RestChannel channel, RestRequest request, Client client, Builder additionalSettings)
-			throws IOException {
+    @Override
+    protected void handleGet(final RestChannel channel, RestRequest request, Client client, Builder additionalSettings) throws IOException {
 
-		String application = request.param("application");
-		String resourcename = request.param("name");
+        String application = request.param("application");
+        String resourcename = request.param("name");
 
-		if (Strings.isNullOrEmpty(application) || !Strings.isNullOrEmpty(resourcename)) {
-			super.handleGet(channel, request, client, additionalSettings);
-		}
+        if (Strings.isNullOrEmpty(application) || !Strings.isNullOrEmpty(resourcename)) {
+            super.handleGet(channel, request, client, additionalSettings);
+        }
 
-		boolean negate = false;
+        boolean negate = false;
 
-		if (application.startsWith("-")) {
-			application = application.substring(1);
-			negate = true;
-		}
+        if (application.startsWith("-")) {
+            application = application.substring(1);
+            negate = true;
+        }
 
-		final Settings configurationSettings = filterActionGroupsByApplication(
-				loadAsSettings(getConfigName(), true).v2(), application, negate);
+        final Settings configurationSettings = filterActionGroupsByApplication(loadAsSettings(getConfigName(), true).v2(), application, negate);
 
-		channel.sendResponse(new BytesRestResponse(RestStatus.OK, convertToJson(channel, configurationSettings)));
-	}
+        channel.sendResponse(new BytesRestResponse(RestStatus.OK, convertToJson(channel, configurationSettings)));
+    }
 
-	private Settings filterActionGroupsByApplication(Settings settings, String application, boolean negate) {
-		Settings.Builder resultBuilder = Settings.builder();
+    private Settings filterActionGroupsByApplication(Settings settings, String application, boolean negate) {
+        Settings.Builder resultBuilder = Settings.builder();
 
-		for (Map.Entry<String, Settings> entry : settings.getAsGroups(true).entrySet()) {
-			List<String> matchingPermissions = getMatchingPermissions(settings, entry.getKey(), application,
-					entry.getValue(), negate);
+        for (Map.Entry<String, Settings> entry : settings.getAsGroups(true).entrySet()) {
+            List<String> matchingPermissions = getMatchingPermissions(settings, entry.getKey(), application, entry.getValue(), negate);
 
-			if (matchingPermissions != null) {
-				resultBuilder.putList(entry.getKey() + ".permissions", matchingPermissions);
+            if (matchingPermissions != null) {
+                resultBuilder.putList(entry.getKey() + ".permissions", matchingPermissions);
 
-				if (entry.getValue().hasValue("readonly")) {
-					resultBuilder.put(entry.getKey() + ".readonly", entry.getValue().getAsBoolean("readonly", false));
-				}
-			}
+                if (entry.getValue().hasValue("readonly")) {
+                    resultBuilder.put(entry.getKey() + ".readonly", entry.getValue().getAsBoolean("readonly", false));
+                }
+            }
 
-		}
+        }
 
-		return resultBuilder.build();
-	}
+        return resultBuilder.build();
+    }
 
-	private List<String> getMatchingPermissions(Settings settings, String key, String application, Settings subSettings,
-			boolean negate) {
-		List<String> result = new ArrayList<>(subSettings.size());
+    private List<String> getMatchingPermissions(Settings settings, String key, String application, Settings subSettings, boolean negate) {
+        List<String> result = new ArrayList<>(subSettings.size());
 
-		if (subSettings.size() == 0) {
-			return result;
-		}
+        if (subSettings.size() == 0) {
+            return result;
+        }
 
-		for (String permission : subSettings.getAsList("permissions")) {
-			if (matchPermission(settings, key, permission, application, negate, 10)) {
-				result.add(permission);
-			}
-		}
+        for (String permission : subSettings.getAsList("permissions")) {
+            if (matchPermission(settings, key, permission, application, negate, 10)) {
+                result.add(permission);
+            }
+        }
 
-		if (result.size() > 0) {
-			return result;
-		} else {
-			return null;
-		}
+        if (result.size() > 0) {
+            return result;
+        } else {
+            return null;
+        }
 
-	}
+    }
 
-	private boolean containsActionByApplication(Settings settings, String key, String application, boolean negate,
-			int maxRecursionDepth) {
-		if (maxRecursionDepth <= 0) {
-			log.warn("Max recursion depth exceeded for action group " + key);
-			return false;
-		}
+    private boolean containsActionByApplication(Settings settings, String key, String application, boolean negate, int maxRecursionDepth) {
+        if (maxRecursionDepth <= 0) {
+            log.warn("Max recursion depth exceeded for action group " + key);
+            return false;
+        }
 
-		List<String> permissions = settings.getAsList(key + ".permissions");
+        List<String> permissions = settings.getAsList(key + ".permissions");
 
-		if (permissions == null) {
-			return false;
-		}
+        if (permissions == null) {
+            return false;
+        }
 
-		for (String permission : permissions) {
-			if (matchPermission(settings, key, permission, application, negate, maxRecursionDepth)) {
-				return true;
-			}
-		}
+        for (String permission : permissions) {
+            if (matchPermission(settings, key, permission, application, negate, maxRecursionDepth)) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	private boolean matchPermission(Settings settings, String key, String permission, String application,
-			boolean negate, int maxRecursionDepth) {
-		if (permission == null) {
-			return false;
-		}
+    private boolean matchPermission(Settings settings, String key, String permission, String application, boolean negate, int maxRecursionDepth) {
+        if (permission == null) {
+            return false;
+        }
 
-		if (permission.startsWith(application + ":")) {
-			return true ^ negate;
-		}
+        if (permission.startsWith(application + ":")) {
+            return true ^ negate;
+        }
 
-		if (!key.equals(permission)
-				&& (settings.hasValue(permission) || settings.hasValue(permission + ".permissions"))) {
-			if (containsActionByApplication(settings, permission, application, negate, maxRecursionDepth - 1)) {
-				return true;
-			}
-		}
+        if (!key.equals(permission) && (settings.hasValue(permission) || settings.hasValue(permission + ".permissions"))) {
+            if (containsActionByApplication(settings, permission, application, negate, maxRecursionDepth - 1)) {
+                return true;
+            }
+        }
 
-		return false ^ negate;
-	}
+        return false ^ negate;
+    }
 }
