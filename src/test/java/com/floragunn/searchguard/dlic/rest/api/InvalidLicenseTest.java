53bb2a5a84ae11d313ea4ebf5e97da063e225940
floragunn <dev@search-guard.com>
2019-05-02 21:56:44 +0200

    Update links to the documentation

--- src/test/java/com/floragunn/searchguard/dlic/rest/api/InvalidLicenseTest.java
+++ src/test/java/com/floragunn/searchguard/dlic/rest/api/InvalidLicenseTest.java
@@ -44,7 +44,7 @@ public class InvalidLicenseTest extends LicenseTest {
 		 Assert.assertEquals(Boolean.FALSE.toString(), String.valueOf(settingsAsMap.get("is_valid")));
 		 Assert.assertEquals(expiredStartDate.format(formatter), settingsAsMap.get("start_date"));
 		 Assert.assertEquals(expiredExpiryDate.format(formatter), settingsAsMap.get("expiry_date"));
-		 Assert.assertEquals("Purchase a license. Visit docs.search-guard.com/v6/search-guard-enterprise-edition or write to <sales@floragunn.com>", settingsAsMap.get("action"));
+		 Assert.assertEquals("Purchase a license. Visit docs.search-guard.com/latest/search-guard-enterprise-edition or write to <sales@floragunn.com>", settingsAsMap.get("action"));
 		 Assert.assertEquals("License is expired", ((List)settingsAsMap.get("msgs")).get(0));
 		 Assert.assertEquals("Only 1 node(s) allowed but you run 3 node(s)", ((List)settingsAsMap.get("msgs")).get(1));
 	}
