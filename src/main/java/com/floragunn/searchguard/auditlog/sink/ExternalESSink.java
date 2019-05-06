eb396f61761c3d71f241f61c410123cdbfb223aa
floragunn <dev@search-guard.com>
2019-05-06 20:31:40 +0200

    [BREAKING] change the default of "searchguard.audit.config.type" from "auditlog" to null In case of the default value (null) the default type _doc will be used. This is to reduce deprecation warnings but keep the functionality to store the logs in external ES 6 clusters (in 5.x indices where types still allowed)

--- src/main/java/com/floragunn/searchguard/auditlog/sink/ExternalESSink.java
+++ src/main/java/com/floragunn/searchguard/auditlog/sink/ExternalESSink.java
@@ -65,7 +65,7 @@ public final class ExternalESSink extends AuditLogSink {
                     + "If you have no date pattern configured you can safely ignore this message", e.getMessage());
         }
 		
-		this.type = sinkSettings.get(ConfigConstants.SEARCHGUARD_AUDIT_ES_TYPE, "auditlog");
+		this.type = sinkSettings.get(ConfigConstants.SEARCHGUARD_AUDIT_ES_TYPE, null);
 		final boolean verifyHostnames = sinkSettings.getAsBoolean(ConfigConstants.SEARCHGUARD_AUDIT_EXTERNAL_ES_VERIFY_HOSTNAMES, true);
 		final boolean enableSsl = sinkSettings.getAsBoolean(ConfigConstants.SEARCHGUARD_AUDIT_EXTERNAL_ES_ENABLE_SSL, false);
 		final boolean enableSslClientAuth = sinkSettings.getAsBoolean(ConfigConstants.SEARCHGUARD_AUDIT_EXTERNAL_ES_ENABLE_SSL_CLIENT_AUTH , ConfigConstants.SEARCHGUARD_AUDIT_SSL_ENABLE_SSL_CLIENT_AUTH_DEFAULT);
