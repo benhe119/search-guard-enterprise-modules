ed09cf1f46690a0efde447d000c99e5def47d81f
floragunn <dev@search-guard.com>
2019-07-02 12:47:13 +0200

    Backport https://github.com/floragunncom/search-guard-enterprise-modules/pull/46 for v6.5.4-33.0

--- src/main/java/com/floragunn/searchguard/dlic/rest/api/SgConfigAction.java
+++ src/main/java/com/floragunn/searchguard/dlic/rest/api/SgConfigAction.java
@@ -14,6 +14,7 @@
 
 package com.floragunn.searchguard.dlic.rest.api;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 import org.elasticsearch.client.Client;
@@ -22,76 +23,107 @@ import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.rest.BytesRestResponse;
+import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
-import org.elasticsearch.rest.RestRequest.Method;
 import org.elasticsearch.rest.RestResponse;
+import org.elasticsearch.rest.RestRequest.Method;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.threadpool.ThreadPool;
 
 import com.floragunn.searchguard.auditlog.AuditLog;
 import com.floragunn.searchguard.configuration.AdminDNs;
 import com.floragunn.searchguard.configuration.IndexBaseConfigurationRepository;
+import com.floragunn.searchguard.dlic.rest.support.Utils;
 import com.floragunn.searchguard.dlic.rest.validation.AbstractConfigurationValidator;
-import com.floragunn.searchguard.dlic.rest.validation.NoOpValidator;
+import com.floragunn.searchguard.dlic.rest.validation.SgConfigValidator;
 import com.floragunn.searchguard.privileges.PrivilegesEvaluator;
 import com.floragunn.searchguard.ssl.transport.PrincipalExtractor;
 import com.floragunn.searchguard.support.ConfigConstants;
 
-public class SgConfigAction extends AbstractApiAction {
+public class SgConfigAction extends PatchableResourceApiAction {
 
-	@Inject
-	public SgConfigAction(final Settings settings, final Path configPath, final RestController controller, final Client client,
-			final AdminDNs adminDNs, final IndexBaseConfigurationRepository cl, final ClusterService cs,
-			final PrincipalExtractor principalExtractor, final PrivilegesEvaluator evaluator, ThreadPool threadPool, AuditLog auditLog) {
-		super(settings, configPath, controller, client, adminDNs, cl, cs, principalExtractor, evaluator, threadPool, auditLog);
-		controller.registerHandler(Method.GET, "/_searchguard/api/sgconfig/", this);
-	}
+    private final boolean allowPutOrPatch;
 
+    @Inject
+    public SgConfigAction(final Settings settings, final Path configPath, final RestController controller, final Client client,
+            final AdminDNs adminDNs, final IndexBaseConfigurationRepository cl, final ClusterService cs, final PrincipalExtractor principalExtractor,
+            final PrivilegesEvaluator evaluator, ThreadPool threadPool, AuditLog auditLog) {
+        super(settings, configPath, controller, client, adminDNs, cl, cs, principalExtractor, evaluator, threadPool, auditLog);
 
+        allowPutOrPatch = settings.getAsBoolean(ConfigConstants.SEARCHGUARD_UNSUPPORTED_RESTAPI_ALLOW_SGCONFIG_MODIFICATION, false);
 
-	@Override
-	protected Tuple<String[], RestResponse> handleGet(RestRequest request, Client client,
-			final Settings.Builder additionalSettingsBuilder) throws Throwable {
+        controller.registerHandler(Method.GET, "/_searchguard/api/sgconfig/", this);
 
-		final Settings configurationSettings = loadAsSettings(getConfigName(), true);
+        if (allowPutOrPatch) {
+            controller.registerHandler(Method.PUT, "/_searchguard/api/sgconfig/{name}", this);
+            controller.registerHandler(Method.PATCH, "/_searchguard/api/sgconfig/", this);
+        }
 
-		return new Tuple<String[], RestResponse>(new String[0],
-				new BytesRestResponse(RestStatus.OK, convertToJson(configurationSettings)));
-	}
-	
-	@Override
-	protected Tuple<String[], RestResponse> handlePut(final RestRequest request, final Client client,
-			final Settings.Builder additionalSettings) throws Throwable {
-		return notImplemented(Method.PUT);
-	}
+    }
 
-	@Override
-	protected Tuple<String[], RestResponse> handleDelete(final RestRequest request, final Client client,
-			final Settings.Builder additionalSettings) throws Throwable {
-		return notImplemented(Method.DELETE);
-	}
+    @Override
+    protected Tuple<String[], RestResponse> handleApiRequest(RestRequest request, Client client) throws Throwable {
+        if (request.method() == Method.PATCH && !allowPutOrPatch) {
+            return notImplemented(Method.PATCH);
+        } else {
+            return super.handleApiRequest(request, client);
+        }
+    }
 
-	@Override
-	protected AbstractConfigurationValidator getValidator(RestRequest request, BytesReference ref, Object... param) {
-		return new NoOpValidator(request, ref, this.settings, param);
-	}
+    @Override
+    protected Tuple<String[], RestResponse> handleGet(RestRequest request, Client client, final Settings.Builder additionalSettingsBuilder) throws IOException {
+        final Settings configurationSettings = loadAsSettings(getConfigName(), true);
+        return new Tuple<String[], RestResponse>(new String[0],
+                new BytesRestResponse(RestStatus.OK, convertToJson(configurationSettings)));
+    }
 
-	@Override
-	protected String getConfigName() {
-		return ConfigConstants.CONFIGNAME_CONFIG;
-	}
+    @Override
+    protected Tuple<String[], RestResponse> handlePut(final RestRequest request, final Client client, final Settings.Builder additionalSettings)
+            throws Throwable {
+        if (allowPutOrPatch) {
+            if (!"searchguard".equals(request.param("name"))) {
+                return badRequestResponse("name must be searchguard");
+            }
+            return super.handlePut(request, client, additionalSettings);
+        } else {
+            return notImplemented(Method.PUT);
+        }
+    }
 
-	@Override
-	protected Endpoint getEndpoint() {
-		return Endpoint.SGCONFIG;
-	}
+    @Override
+    protected Tuple<String[], RestResponse> handleDelete(final RestRequest request, final Client client, final Settings.Builder additionalSettings) {
+        return notImplemented(Method.DELETE);
+    }
 
-	@Override
-	protected String getResourceName() {
-		// not needed, no single resource
-		return null;
-	}
+    @Override
+    protected Tuple<String[], RestResponse> handlePost(final RestRequest request, final Client client, final Settings.Builder additionalSetting)
+            throws IOException {
+        return notImplemented(Method.POST);
+    }
+
+    @Override
+    protected AbstractConfigurationValidator getValidator(RestRequest request, BytesReference ref, Object... param) {
+        return new SgConfigValidator(request, ref, this.settings, param);
+    }
+
+    @Override
+    protected String getConfigName() {
+        return ConfigConstants.CONFIGNAME_CONFIG;
+    }
+
+    @Override
+    protected Endpoint getEndpoint() {
+        return Endpoint.SGCONFIG;
+    }
+
+    @Override
+    protected String getResourceName() {
+        // not needed, no single resource
+        return null;
+    }
 
 }
